316c7c742847470de1ed82c451db64109af1e8f2
Xavier Araújo <xavier.araujo@fanduel.com>
2023-08-11 15:02:30 +0100

    add project skeleton

---
+++ src/main/java/com/araujo/xavier/kafka/fundamentals/training/KafkaFundamentalsTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.araujo.xavier.kafka.fundamentals.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFundamentalsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFundamentalsTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/araujo/xavier/kafka/fundamentals/training/KafkaFundamentalsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araujo.xavier.kafka.fundamentals.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFundamentalsTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
