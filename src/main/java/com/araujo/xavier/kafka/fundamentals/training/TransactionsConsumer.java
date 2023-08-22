382fa7e0f3b6d6e6b02af4a33e1cb65686aee086
Xavier Araújo <xavier.araujo@fanduel.com>
2023-08-22 08:55:16 +0100

    add reactor-kafka implementation for producer and consumer

--- src/main/java/com/araujo/xavier/kafka/fundamentals/training/TransactionsConsumer.java
+++ src/main/java/com/araujo/xavier/kafka/fundamentals/training/TransactionsConsumer.java
@@ -1,7 +1,19 @@
 package com.araujo.xavier.kafka.fundamentals.training;
 
+import com.araujo.xavier.kafka.fundamentals.training.serialization.JacksonDeserializer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import reactor.core.scheduler.Schedulers;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 @Slf4j
@@ -17,6 +29,34 @@ public class TransactionsConsumer {
     }
 
     private void start() {
-        // TODO: Implement a basic Kafka consumer
+        log.info("Initializing consumer for topic {} and consumer-group {}", topic, properties.get("group.id"));
+
+        Map<String, Object> consumerProps = new HashMap<>();
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("bootstrap.servers"));
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, properties.get("group.id"));
+
+        ReceiverOptions<String, AccountTransaction> receiverOptions =
+                ReceiverOptions.<String, AccountTransaction>create(consumerProps)
+                        .withKeyDeserializer(new StringDeserializer())
+                        .withValueDeserializer(new JacksonDeserializer<>(AccountTransaction.class))
+                        .maxDeferredCommits(100) // Allows for unordered commits! The framework will handle this out of the box.
+                        .subscription(Collections.singleton(topic));
+
+        KafkaReceiver.create(receiverOptions)
+                .receive()
+                .publishOn(Schedulers.boundedElastic())
+                .doOnNext(r -> {
+                    boolean successfulHandling = handleAccountTransaction(r.value());
+                    if (successfulHandling) {
+                        r.receiverOffset().commit().block(); // allows to control the way we commit offsets
+                    }
+                })
+                .retryWhen(Retry.backoff(3, Duration.of(10L, ChronoUnit.SECONDS)))
+                .subscribe();
+    }
+
+    private boolean handleAccountTransaction(AccountTransaction accountTransaction) {
+        log.info("handling account transaction {}", accountTransaction);
+        return true;
     }
 }
--- src/main/java/com/araujo/xavier/kafka/fundamentals/training/TransactionsProducer.java
+++ src/main/java/com/araujo/xavier/kafka/fundamentals/training/TransactionsProducer.java
@@ -1,22 +1,63 @@
 package com.araujo.xavier.kafka.fundamentals.training;
 
+import com.araujo.xavier.kafka.fundamentals.training.serialization.JacksonSerializer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
 
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class TransactionsProducer {
 
-	private final String topic;
+    private final String topic;
 
-	private final Properties properties;
+    private final Properties properties;
 
-	TransactionsProducer(String topic, Properties properties) {
-		this.topic = topic;
-		this.properties = properties;
-	}
+    TransactionsProducer(String topic, Properties properties) {
+        this.topic = topic;
+        this.properties = properties;
+    }
 
-	private void start() {
-		// TODO: Implement a basic Kafka producer
-	}
+    private void start() {
+        log.info("Initializing producer for topic {}", topic);
+
+        Map<String, Object> producerProps = new HashMap<>();
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("bootstrap.servers"));
+
+        SenderOptions<String, AccountTransaction> senderOptions = SenderOptions
+                .<String, AccountTransaction>create(producerProps)
+                .withKeySerializer(new StringSerializer())
+                .withValueSerializer(new JacksonSerializer<>())
+                .maxInFlight(1024); // Controls back-pressure
+
+		KafkaSender.create(senderOptions)
+				.createOutbound()
+                .send(Flux.interval(Duration.ofSeconds(1))
+                        .map(ignored -> generateRandomTransaction())
+                        .map(transaction -> new ProducerRecord<>(topic, transaction.accountId(), transaction)))
+                .then()
+				.doOnError(Throwable::printStackTrace)
+				.subscribe();
+    }
+
+    private AccountTransaction generateRandomTransaction() {
+        return new AccountTransaction(
+                String.valueOf(ThreadLocalRandom.current().nextLong(0, 10)),
+                UUID.randomUUID().toString(),
+                Timestamp.from(Instant.now()).getTime(),
+                ThreadLocalRandom.current().nextLong(-100, 100)
+        );
+    }
 }
