b0916d9038e8c4f7c5b80fceacff7259ba1bb0c6
Xavier Araújo <xavier.araujo@fanduel.com>
2025-05-19 10:15:35 +0100

    tweaks on docker compose

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -9,3 +9,4 @@ sleep 30
 echo "Setting up Kafka topics"
 docker cp ./scripts/kafka-setup.sh kafka-fundamentals-training-kafka:/kafka-setup.sh
 docker exec -it kafka-fundamentals-training-kafka /kafka-setup.sh
+echo "Finished setting up training data"
