56aaf64e80f80c0ecf334937ba58f4141d919d6e
Xavier Araújo <xavier.araujo@fanduel.com>
2025-05-19 10:19:21 +0100

    tweaks on docker compose

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -9,4 +9,11 @@ sleep 30
 echo "Setting up Kafka topics"
 docker cp ./scripts/kafka-setup.sh kafka-fundamentals-training-kafka:/kafka-setup.sh
 docker exec -it kafka-fundamentals-training-kafka /kafka-setup.sh
-echo "Finished setting up training data"
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo ""
+echo ""
+echo "${GREEN}#####################################${NC}"
+echo "${GREEN}# Finished setting up training data #${NC}"
+echo "${GREEN}#####################################${NC}"
